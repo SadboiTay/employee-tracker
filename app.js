252d0b03f8192253d5b8326617bc7015a815b13e
SadboiTay <tg.hakes@gmail.com>
2021-10-10 12:51:53 -0600

    remove mock objects

--- app.js
+++ app.js
@@ -7,24 +7,6 @@ const employee = new Employee;
 const Prompt = require('./lib/prompts');
 const prompt = new Prompt;
 
-const garbageMan = {
-    title: 'Garbage Man',
-    salary: 15000.00,
-    department_id: 5
-}
-
-const todd = {
-    first_name: 'Todd',
-    last_name: 'Packer',
-    role_id: 4,
-    manager_id: 1
-}
-
-const newrole = {
-    employee_id: 1,
-    role_id: 15
-}
-
 const initialize = () => {
     inquirer.prompt([
         {
