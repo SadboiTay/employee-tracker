cfd8a92951219b0352d8c7e2c58280cb107f6c9e
SadboiTay <tg.hakes@gmail.com>
2021-10-10 12:50:19 -0600

    reset db

--- db/rebuild.sql
+++ db/rebuild.sql
@@ -2,4 +2,12 @@ source db/db.sql;
 source db/schema.sql;
 source db/seeds.sql;
 
-SELECT CONCAT(first_name,' ', last_name) AS name, id FROM employee;
+SELECT e.id, e.first_name, e.last_name, r.title AS title, d.name AS department, r.salary AS salary, CONCAT(mngr.first_name, ' ', mngr.last_name, ' ') AS manager
+FROM employee AS e
+JOIN role AS r
+ON e.role_id =  r.id
+JOIN department AS d
+ON r.department_id = d.id
+LEFT JOIN employee AS mngr
+ON e.manager_id = mngr.id
+ORDER BY e.id;
